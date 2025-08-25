08bbbce2f37a915fae91989a0f7ed95c00efaa65
Marko Damjanac <36675797+markodam5@users.noreply.github.com>
2025-08-25 16:18:32 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 define("DBHOST",'localhost'); 
 define("DBUSER","root");
 define("DBPASS","");
-define("DBNAME","cars");
+define("DBNAME","base");
 
 $conn = @mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
